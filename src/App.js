0f6c698ec9b57a2216045b7f0cba801e2b5ecced
Joselo Mantilla <mantilla.carlosjoselo@gmail.com>
2022-03-20 23:51:13 -0500

    Proyect end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Card from './components/Card.js'
+import Container from './components/Container.js'
+import UserForm from './components/UserForm.js'
 
 function App() {
+  const [usuarios, setUsuarios] = useState([])
+  
+  
+  const submit = usuario =>{
+    setUsuarios([
+      ...usuarios,
+      usuario,
+    ])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{marginTop: '15%'}}>
+      <Container>
+        <Card>
+          <div style={{padding:'20px'}}>
+            <UserForm submit={submit}/>
+          </div>
+        </Card>
+        <Card>
+          <ul>
+            {usuarios.map(x => 
+              <li key={x.email}>{`${x.name} ${x.lastname}: ${x.email}`}</li>
+              )}
+          </ul>
+        </Card>
+      </Container>
     </div>
   );
 }
